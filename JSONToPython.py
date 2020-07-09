4a16cf39cf64a8bfe48075404bb33fabd579e6a7
Yifei-G <yifei.ge91@gmail.com>
2020-07-09 12:51:36 +0200

    added jsonTOPython logic to fully convert a json string to Python object (Vehicle)

--- JSONToPython.py
+++ JSONToPython.py
@@ -6,9 +6,46 @@ def jsonTOPython(jsonData):
     try:
         # new_car will be a python dict variable.
         # Converting it to Vehicle object
-        new_car = json.loads(jsonData)
-
+        new_carJSON = json.loads(jsonData)
         # TODO: evaluating each key-value pairs of the dict is the data are
         # valid to be parsed as a Python object
+        try:
+            regist_num = new_carJSON["registration_number"]
+            assert (regist_num.isalnum()) and (len(regist_num) == 8)
+        except ValueError:
+            print("Registration number is not valid!!")
+            exit(1)
+
+        try:
+            product_year = new_carJSON["year_of_production"]
+            assert (product_year.isdigit()) and (len(product_year) == 4)
+        except AssertionError:
+            print("The year of production is not in the valid format")
+            exit(2)
+
+        try:
+            isPassenger = new_carJSON["passenger"]
+            assert (isPassenger == True) or (isPassenger == False)
+        except AssertionError:
+            print("The Passenger can be only True or False!!")
+            exit(3)
+
+        try:
+            vehicle_mass = new_carJSON["mass"]
+            assert vehicle_mass > 900
+        except ValueError:
+            print("You didn't enter a numeric value")
+            exit(4)
+        except AssertionError:
+            print("Opss your vehicle is so light!!")
+            exit(4)
+
+        new_car = Vehicle(regist_num, product_year, isPassenger, vehicle_mass)
+        print("This is the car information:")
+        print("Registration number:", new_car.getRegist_num())
+        print("Year of production:", new_car.getProduct_year())
+        print("Passenger:", new_car.isPassenger())
+        print("Mass:", new_car.getVehicle_mass())
+
     except json.JSONDecodeError:
         print("Can't convert JSON object to Python object!")
--- Vehicle.py
+++ Vehicle.py
@@ -4,13 +4,25 @@ import json
 class Vehicle:
 
     def __init__(self, regist_num, product_year, isPassenger, vehicle_mass):
-        self.regist_num = regist_num
-        self.product_year = product_year
+        self.registration_number = regist_num
+        self.year_of_production = product_year
         self.passenger = isPassenger
-        self.vehicle_mass = vehicle_mass
+        self.mass = vehicle_mass
 
     def jsonEncoder(car):
         if isinstance(car, Vehicle):
             return car.__dict__
         else:
             raise TypeError(car.__class__.__name__ + "is not JSON seriazable")
+
+    def getRegist_num(self):
+        return self.registration_number
+
+    def getProduct_year(self):
+        return self.year_of_production
+
+    def isPassenger(self):
+        return self.passenger
+
+    def getVehicle_mass(self):
+        return self.mass
